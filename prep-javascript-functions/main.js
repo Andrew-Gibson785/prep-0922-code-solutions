505f9f3b5caeebf8aedab996d8265fb209352dca
Andrew <andrewgibson785@yahoo.com>
2022-09-29 20:26:07 +0000

    Completed prep-javascript-functions exercise - Andrew Gibson

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,53 @@
+function addTwoNumbers(x, y) {
+  return x + y;
+}
+var sum = addTwoNumbers(2, 2);
+console.log('sum', sum);
+
+function converthourstominutes(time) {
+  return time * 60;
+}
+var answer = converthourstominutes(2);
+console.log(answer);
+
+function getGreeting(name) {
+  return name;
+}
+var greeting = getGreeting('Hello World!');
+console.log(greeting);
+
+function addAndMultiplyBy5(a, b) {
+  return (a + b) * 5;
+}
+var total = addAndMultiplyBy5(10, 5);
+console.log('The answer is', total);
+
+function multiplyAndDivideBy5(c, d) {
+  return c * d / 5;
+}
+var amount = multiplyAndDivideBy5(35, 10);
+console.log('The answer is', amount);
+
+function subtractTwoNumbers(e, f) {
+  return e - f;
+}
+var response = subtractTwoNumbers(22, 7);
+console.log('The answer is', response);
+
+function getCircleCircumference(g) {
+  return 2 * 3.14 * g;
+}
+var radius = getCircleCircumference(5);
+console.log('The answer is', radius);
+
+function getFullName(firstName, lastName) {
+  return firstName + ' ' + lastName;
+}
+var fullName = getFullName('Andrew', 'Gibson');
+console.log('The full name is', fullName);
+
+function cube(f) {
+  return f * f * f;
+}
+var reply = cube(5);
+console.log('The answer is', reply);
